c72c5110378e447bb245f7a8b3d0ea59fb4a8813
Gautham Vanjre <vanjregautham@gmail.com>
2023-05-19 21:28:10 +0530

    implemented firebase config and make it encrypted

---
+++ src/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import "firebase/storage";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { apiKey, appId, authDomain, measurementId, messagingSenderId, projectId, storageBucket } from "../src/firebase-config";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const storage = getStorage(app);
+export const db = getFirestore(app);
