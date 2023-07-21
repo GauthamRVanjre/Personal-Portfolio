23be2bad54065a73f83e5c6744b4caf73f21169a
Gautham R Vanjre <77773646+GauthamRVanjre@users.noreply.github.com>
2023-07-21 20:17:30 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const TimeLineData = [
   { year: "2019", text: "Started my bachelor's degree" },
-  { year: "08-2023", text: "FSuccessfully Finished my bachelor's degree" },
+  { year: "08-2023", text: "Successfully Finished my bachelor's degree" },
   { year: "03-2023 - Present", text: "Software Engineering Intern @AlphaBi" },
 ];
