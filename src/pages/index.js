88857cdacdce7512db4c79a3094915a25432d9a2
Gautham Vanjre <vanjregautham@gmail.com>
2023-10-17 20:01:05 +0530

    solved build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation"
 import ContactMe from "../components/ContactMe/ContactMe";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
+import SkillSet from "../components/SkillsSet/SkillSet";
 import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
@@ -16,7 +17,7 @@ const Home = () => {
       {/* <BgAnimation /> */}
       {/* </Section> */}
       <Timeline />
-      <SkillsSet />
+      <SkillSet />
       <Projects />
       <Technologies />
       <Acomplishments />
